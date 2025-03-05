7fd0bf2b07356215aa2311502e53bbbb827209a1
Mr-Thop <karthikreddy0314@gmail.com>
2025-03-05 22:59:31 +0530

    Creating File Card.Java

---
+++ Card.java
@@ -0,0 +1,38 @@
+// Name: Karne Karthikeshwar Reddy
+// PRN: 23070126055
+// Batch: SY - AIML A3
+
+public class Card {
+    private String suit;
+    private String rank;
+
+    // This is the Constructor
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    // Getters
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    // Display card details
+    public void printCard() {
+        System.out.println(rank + " of " + suit);
+    }
+
+    // Compare if two cards have the same suit
+    public boolean sameCard(Card other) {
+        return this.suit.equals(other.suit);
+    }
+
+    // Compare if two cards have the same rank
+    public boolean compareCard(Card other) {
+        return this.rank.equals(other.rank);
+    }
+}
