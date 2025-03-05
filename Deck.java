d247713d1a6fa071f5f0835f5a02ce5301b64cbb
Mr-Thop <karthikreddy0314@gmail.com>
2025-03-05 23:00:08 +0530

    Creating File Deck.Java

---
+++ Deck.java
@@ -0,0 +1,61 @@
+// Name: Karne Karthikeshwar Reddy
+// PRN: 23070126055
+// Batch: SY - AIML A3
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class Deck {
+    private ArrayList<Card> deck; // Stores the deck of cards
+    private static final String[] SUITS = {"Hearts", "Diamonds", "Clubs", "Spades"};
+    private static final String[] RANKS = {
+        "2", "3", "4", "5", "6", "7", "8", "9", "10", 
+        "Jack", "Queen", "King", "Ace"
+    };
+
+    // Constructor to initialize the deck
+    public Deck() {
+        createDeck();
+    }
+
+    // Create a deck of 52 cards
+    public void createDeck() {
+        deck = new ArrayList<>();
+        for (String suit : SUITS) {
+            for (String rank : RANKS) {
+                deck.add(new Card(suit, rank));
+            }
+        }
+    }
+
+    // Print all cards in the deck
+    public void printDeck() {
+        for (Card card : deck) {
+            card.printCard();
+        }
+    }
+
+    // Find a specific card in the deck
+    public boolean findCard(String rank, String suit) {
+        for (Card card : deck) {
+            if (card.getRank().equalsIgnoreCase(rank) && card.getSuit().equalsIgnoreCase(suit)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Shuffle the deck
+    public void shuffleDeck() {
+        Collections.shuffle(deck);
+    }
+
+    // Deal 5 random cards
+    public void dealCard() {
+        Random rand = new Random();
+        for (int i = 0; i < 5; i++) {
+            deck.get(rand.nextInt(deck.size())).printCard();
+        }
+    }
+}
